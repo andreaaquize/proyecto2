779e793721ce8b0849ab803eda5f98ebf20bcc63
andreaaquize <51956799+andreaaquize@users.noreply.github.com>
2019-06-18 07:26:24 -0500

    Add files via upload

---
+++ proyecto.py
@@ -0,0 +1,165 @@
+import os
+
+from math import sqrt
+
+
+def redondeo (boaty):
+
+    conver = int(boaty)
+
+    if boaty-conver >= 0.5:
+        conver =conver +1
+
+    return conver
+
+
+def llenado (canvas):
+
+   llenar =["."]
+   borde=["."]
+
+   for i in range (80):
+        if i != 79:
+             llenar.append(" ")
+        else:
+             llenar.append(".")
+
+        borde.append(".")
+
+   for i in range (42):
+       if i ==0 or i==41:
+          canvas.append(borde.copy())
+       else:
+          canvas.append(llenar.copy())
+
+   return canvas
+
+
+
+def linea (x1,y1,x2,y2, canvas):
+
+    if x1 == x2:
+        for k in range (y1,y2 +1):
+            canvas[42-k].pop(x1)
+            canvas[42-k].insert(x1,"*")
+
+    elif y1==y2:
+
+        for k in range (x1,x2+1):
+            canvas[42-y1].pop(k)
+            canvas[42-y1].insert(k,"*")
+
+    else:
+
+        m= (y2-y1)/(x2-x1)
+
+        b=y1-m*x1
+        for k in range (x1,x2 + 1):
+            y=int(m*k + b)
+            canvas[42-y].pop(k)
+            canvas[42-y].insert(k,"*")
+
+
+    return canvas
+
+
+
+def rectangulo (x1,y1,b,h,canvas):
+
+    linea(x1,y1,x1+b,y1,canvas)
+    linea(x1,y1+h,x1+b,y1+h,canvas)
+    linea(x1,y1,x1,y1+h,canvas)
+    linea(x1+b,y1,x1+b,y1+h,canvas)
+
+
+
+def triángulo (x1,y1,b,h,canvas):
+
+    linea(x1,y1,x1+b,y1,canvas)
+
+    a=redondeo(x1+b/2)
+    linea(x1,y1,a,y1+h,canvas)
+    linea(a,y1+h,x1+b,y1,canvas)
+
+
+def circulo (x1,y1,rad,canvas):
+
+    for k in range (x1-rad, x1+rad+1):
+
+        y2=redondeo(y1 + sqrt(rad*rad-(k-x1)*(k-x1)))
+        y3=redondeo(y1- sqrt(rad*rad-(k-x1)*(k-x1)))
+
+        if y2==y3:
+            canvas[42-y2].pop(k)
+            canvas[42-y2].insert(k,"*")
+        else:
+            canvas[42-y2].pop(k)
+            canvas[42-y3].pop(k)
+            canvas[42-y2].insert(k,"*")
+            canvas[42-y3].insert(k,"*")
+
+    return canvas
+
+def elipse (fx1,fy1,fx2,fy2,e,canvas):
+
+    if fy2==fy1:
+
+        alfa=(fx1+fx2)/2
+        beta=fy1
+        c=fx2-alfa
+        a= c/e
+        b= sqrt(a**2 - c**2)
+
+        lim1= redondeo(b+alfa)
+        lim2= redondeo(alfa-b)
+
+        for k in range(lim2,lim1+1):
+
+            y1 =redondeo(beta- sqrt(b**2(1- ((k-alfa)**2)/(a**2))))
+            y2 =redondeo(beta+ sqrt(b**2(1- ((k-alfa)**2)/(a**2))))
+
+            canvas[42-y1].pop(k)
+            canvas[42-y2].pop(k)
+            canvas[42-y2].insert(k,"*")
+            canvas[42-y1].insert(k,"*")
+
+    elif fx1==fx2:
+
+        alfa=fx1
+        beta=(fy1+fy2)/2
+        c= fy2-beta
+        a=c/e
+        b=sqrt(a**2 - c**2)
+        lim1= redondeo(b+alfa)
+        lim2= redondeo(alfa-b)
+
+        for k in range(lim1,lim2+1):
+
+            y1 =redondeo(beta- sqrt(a**2(1- ((k-alfa)**2)/(b**2))))
+            y2 =redondeo(beta+ sqrt(a**2(1- ((k-alfa)**2)/(b**2))))
+
+            canvas[42-y1].pop(k)
+            canvas[42-y2].pop(k)
+            canvas[42-y2].insert(k,"*")
+            canvas[42-y1].insert(k,"*")
+
+
+
+
+
+
+
+def printingCANVAS (canvas):
+    for i in range (42):
+       for j in range (81):
+          print(canvas [i] [j], end="")
+       print()
+
+
+
+
+
+
+
+
+
---
+++ proyectoLlamadas.py
@@ -0,0 +1,89 @@
+from proyecto import llenado
+from proyecto import elipse
+from proyecto import linea
+from proyecto import circulo
+from proyecto import rectangulo
+from proyecto import triángulo
+from proyecto import printingCANVAS
+
+canvas1=[]
+#archivo= open(str(os.getcwd()) + "\\" + nombre + "_Encriptado" +  ".txt", "w")
+
+print("1. Agregar una línea")
+print("2. Agregar una elipse o circulo")
+print("3. Agregar un rectángulo o cuadrado")
+print("4. Agregar un triángulo")
+
+inst=int(input("Ingrese su instrucción: "))
+
+if inst==1:
+
+    x3=int(input("Ingrese la coordenada x de su primer punto: "))
+
+    y3=int(input("Ingrese la coordenada y de su primer punto: "))
+
+    x4=int(input("Ingrese la coordenada x de su segundo punto: "))
+
+    y4=int(input("Ingrese la coordenada y de su segundo punto: "))
+
+    canvas1=llenado(canvas1)
+    canvas1=linea(x3,y3,x4,y4,canvas1)
+    printingCANVAS(canvas1)
+
+if inst==2:
+
+    dif= input("¿Desea dibujar una elipse o un círculo? ")
+
+    if dif=="elipse":
+
+        fx3 = int(input("Ingrese la coordenada x de su primer foco: "))
+        fy3 = int(input("Ingrese la coordenada y de su primer foco: "))
+        fx4 = int(input("Ingrese la coordenada x de su segundo foco: "))
+        fy4 = int(input("Ingrese la coordenada y de su segundo foco: "))
+        e1 = float(input("Ingrese la excentricidad de su elipse: "))
+
+        canvas1=llenado(canvas1)
+        canvas1= elipse(fx3,fy3,fx4,fy4,e1,canvas1)
+        printingCANVAS(canvas1)
+
+
+
+    elif dif=="círculo":
+
+        x3=int(input("Ingrese la coordenada x del centro de su círculo: "))
+        y3=int(input("Ingrese la coordenada y del centro de su círculo: "))
+        radio=int(input("Ingrese el radio de su círculo: "))
+
+        canvas1=llenado(canvas1)
+        canvas1=circulo(x3,y3,radio,canvas1)
+        printingCANVAS(canvas1)
+
+
+
+
+if inst==3:
+   x1=int(input("Ingrese la coordenada x de su primer punto: "))
+   y1=int(input("Ingrese la coordenada y de su primer punto: "))
+   b=int(input("Ingrese su base: "))
+   h = int(input("Ingrese su altura: "))
+
+   canvas1 = llenado(canvas1)
+   canvas1 = rectangulo(x1, y1, b, h, canvas1)
+   printingCANVAS(canvas1)
+
+
+
+if inst==4:
+   x1=int(input("Ingrese la coordenada x de su primer punto: "))
+   y1=int(input("Ingrese la coordenada y de su primer punto: "))
+   b=int(input("Ingrese su base: "))
+   h = int(input("Ingrese su altura: "))
+
+canvas1 = llenado(canvas1)
+canvas1 = triángulo(x1,y1,b,h,canvas1)
+printingCANVAS(canvas1)
+
+
+
+
+
