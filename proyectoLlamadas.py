9cb26cd21eb6d70803e9a043508aa78eaf20e7d3
andreaaquize <51956799+andreaaquize@users.noreply.github.com>
2019-06-18 08:34:40 -0500

    Add files via upload

--- proyectoLlamadas.py
+++ proyectoLlamadas.py
@@ -9,32 +9,40 @@ from proyecto import printingCANVAS
 canvas1=[]
 #archivo= open(str(os.getcwd()) + "\\" + nombre + "_Encriptado" +  ".txt", "w")
 
-print("1. Agregar una línea")
-print("2. Agregar una elipse o circulo")
-print("3. Agregar un rectángulo o cuadrado")
-print("4. Agregar un triángulo")
+inst=0
 
-inst=int(input("Ingrese su instrucción: "))
+while inst!=5:
 
-if inst==1:
+  print("1. Agregar una línea")
+  print("2. Agregar una elipse o circulo")
+  print("3. Agregar un rectángulo o cuadrado")
+  print("4. Agregar un triángulo")
+  print("5. Terminar el programa")
 
-    x3=int(input("Ingrese la coordenada x de su primer punto: "))
+  inst=int(input())
 
-    y3=int(input("Ingrese la coordenada y de su primer punto: "))
+  if inst==1:
 
-    x4=int(input("Ingrese la coordenada x de su segundo punto: "))
+     x3=int(input("Ingrese la coordenada x de su primer punto: "))
 
-    y4=int(input("Ingrese la coordenada y de su segundo punto: "))
+     y3=int(input("Ingrese la coordenada y de su primer punto: "))
 
-    canvas1=llenado(canvas1)
-    canvas1=linea(x3,y3,x4,y4,canvas1)
-    printingCANVAS(canvas1)
+     x4=int(input("Ingrese la coordenada x de su segundo punto: "))
 
-if inst==2:
+     y4=int(input("Ingrese la coordenada y de su segundo punto: "))
+
+     canvas1=llenado(canvas1)
+     canvas1=linea(x3,y3,x4,y4,canvas1)
+     printingCANVAS(canvas1)
+
+  if inst==2:
 
     dif= input("¿Desea dibujar una elipse o un círculo? ")
 
     if dif=="elipse":
+
+        #La elipse no es perfectamente funcional, ya que nadie del grupo había visto elipses antes,
+        # entonces detro del código existe un error de índole matemático, mas no de programación.
 
         fx3 = int(input("Ingrese la coordenada x de su primer foco: "))
         fy3 = int(input("Ingrese la coordenada y de su primer foco: "))
@@ -61,27 +69,27 @@ if inst==2:
 
 
 
-if inst==3:
-   x1=int(input("Ingrese la coordenada x de su primer punto: "))
-   y1=int(input("Ingrese la coordenada y de su primer punto: "))
-   b=int(input("Ingrese su base: "))
-   h = int(input("Ingrese su altura: "))
+  if inst==3:
+     x1=int(input("Ingrese la coordenada x de su primer punto: "))
+     y1=int(input("Ingrese la coordenada y de su primer punto: "))
+     b=int(input("Ingrese su base: "))
+     h = int(input("Ingrese su altura: "))
 
-   canvas1 = llenado(canvas1)
-   canvas1 = rectangulo(x1, y1, b, h, canvas1)
-   printingCANVAS(canvas1)
+     canvas1 = llenado(canvas1)
+     canvas1 = rectangulo(x1, y1, b, h, canvas1)
+     printingCANVAS(canvas1)
 
 
 
-if inst==4:
-   x1=int(input("Ingrese la coordenada x de su primer punto: "))
-   y1=int(input("Ingrese la coordenada y de su primer punto: "))
-   b=int(input("Ingrese su base: "))
-   h = int(input("Ingrese su altura: "))
+  if inst==4:
+     x1=int(input("Ingrese la coordenada x de su primer punto: "))
+     y1=int(input("Ingrese la coordenada y de su primer punto: "))
+     b=int(input("Ingrese su base: "))
+     h = int(input("Ingrese su altura: "))
 
-   canvas1 = llenado(canvas1)
-   canvas1 = triángulo(x1,y1,b,h,canvas1)
-   printingCANVAS(canvas1)
+     canvas1 = llenado(canvas1)
+     canvas1 = triángulo(x1,y1,b,h,canvas1)
+     printingCANVAS(canvas1)
 
 
 
